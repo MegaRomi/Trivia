674bfa33aba5919fcc0aa65503fa3f4ea8c445b2
User <grimberg.romi@gmail.com>
2025-03-11 17:11:28 +0200

    chsaojh

---
+++ app/src/main/java/com/example/trivia11a/instructions.java
@@ -0,0 +1,21 @@
+package com.example.trivia11a;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class instructions extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_instructions);
+
+    }
+}
